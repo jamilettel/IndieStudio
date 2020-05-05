dd901fa084ec0b7505a2ea4996ebb17d3b40dc89
Jamil ETTEL <jamil.ettel@epitech.eu>
2020-05-05 19:19:15 +0200

    :construction: added basic Entity and Component class

---
+++ include/ECS/Component.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2020
+** Indie Studio
+** File description:
+** Component
+*/
+
+#ifndef COMPONENT_HPP_
+#define COMPONENT_HPP_
+
+namespace is::ecs {
+
+    class Entity;
+
+    class Component {
+    public:
+        explicit Component(Entity &e);
+        virtual ~Component() = default;
+
+        Component(const Component &) = delete;
+        Component &operator=(const Component &) = delete;
+
+    protected:
+        Entity &e;
+
+    };
+
+}
+
+#endif /* !COMPONENT_HPP_ */
---
+++ src/ECS/Entity.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** Indie Studio
+** File description:
+** Entity
+*/
+
+#include "ECS/Entity.hpp"
+
+is::ecs::Entity::Entity(): _actif(true), _delete(false)
+{
+    uuid_generate_random(_uuid);
+}
