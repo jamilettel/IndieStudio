06a336a25f4519cf5c535e681854f1a60f196be4
Benjamin Bourgeois <benjishift@gmail.com>
2020-05-10 12:09:55 +0200

    :construction: delete test

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,6 @@
 */
 
 #include "Game.hpp"
-#include "Scenes/SplashScreenScene.hpp"
-#include "Scenes/MainMenuScene.hpp"
 #include "Scenes/GameScene.hpp"
 
 int main(int argc, char const *argv[])
@@ -15,10 +13,8 @@ int main(int argc, char const *argv[])
     is::Game game;
 
     try {
-        game.addScene(is::Game::Scenes::SCENE_SPLASH_SCREEN, std::make_shared<is::scenes::SplashScreenScene>());
-        game.addScene(is::Game::Scenes::SCENE_MAIN_MENU, std::make_shared<is::scenes::MainMenuScene>());
         game.addScene(is::Game::Scenes::SCENE_GAME, std::make_shared<is::scenes::GameScene>());
-        game.launchGame(is::Game::Scenes::SCENE_MAIN_MENU);
+        game.launchGame(is::Game::Scenes::SCENE_GAME);
     } catch (const std::exception &e) {
         std::cerr << e.what() << std::endl;
         return 1;
