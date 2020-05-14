3a32b2a2341b0ba0f83051176baa40d68683c549
Amaury <amaury.lecomte@epitech.eu>
2020-05-14 10:16:03 +0200

    :art: Improve code for AStarAlgorithm, change class Node to struct node

--- include/AStarAlgorithm.hpp
+++ include/AStarAlgorithm.hpp
@@ -16,52 +16,66 @@
 #include <functional>
 #include <memory>
 
-class Node
-{
-    public:
-        Node(std::shared_ptr<Node> parent, std::pair<int, int> pos, double g, double h, double f) : _parent(parent), _pos(pos), _g(g), _h(h), _f(f) {
+// class Node
+// {
+//     public:
+//         Node(std::shared_ptr<Node> parent, std::pair<int, int> pos, double g, double h, double f) : _parent(parent), _pos(pos), _g(g), _h(h), _f(f) {
 
-        };
+//         };
 
-        ~Node() = default;
-        bool operator<(const Node &Node)
-        {
-            return (_f < Node._f);
-        };
-        std::shared_ptr<Node> getParent() const noexcept
-        {
-            return (_parent);
-        };
-        const std::pair<int, int> &getPos() const noexcept
-        {
-            return (_pos);
-        };
-        double getG() const noexcept
-        {
-            return (_g);
-        };
+//         ~Node() = default;
+//         bool operator<(const Node &Node)
+//         {
+//             return (_f < Node._f);
+//         };
+//         std::shared_ptr<Node> parent() const noexcept
+//         {
+//             return (_parent);
+//         };
+//         const std::pair<int, int> &pos() const noexcept
+//         {
+//             return (_pos);
+//         };
+//         double g() const noexcept
+//         {
+//             return (_g);
+//         };
 
-        double getF() const noexcept
-        {
-            return (_f);
-        };
-        double getH() const noexcept
-        {
-            return (_h);
-        };
-    private:
-        std::shared_ptr<Node> _parent;
-        std::pair<int, int> _pos;
-        double _g;
-        double _h;
-        double _f;
-};
+//         double getF() const noexcept
+//         {
+//             return (_f);
+//         };
+//         double getH() const noexcept
+//         {
+//             return (_h);
+//         };
+//     private:
+//         std::shared_ptr<Node> _parent;
+//         std::pair<int, int> _pos;
+//         double _g;
+//         double _h;
+//         double _f;
+// };
 
 namespace is
 {
     template<typename T>
     class AStarAlgorithm {
         public:
+            struct Node
+            {
+                Node(std::shared_ptr<Node> pparent, std::pair<int, int> ppos, double gg, double hh, double ff) : parent(pparent), pos(ppos), g(gg), h(hh), f(ff)
+                {};
+                std::shared_ptr<Node> parent;
+                std::pair<int, int> pos;
+                double g;
+                double h;
+                double f;
+                bool operator<(const Node &node)
+                {
+                    return (f < node.f);
+                }
+            };
 
         public:
             AStarAlgorithm(
@@ -103,19 +117,19 @@ namespace is
                     _openList.erase(ptr);
                     _closeList.emplace_back(currentNode);
 
-                    if (currentNode->getPos().first == endNode->getPos().first && currentNode->getPos().second == endNode->getPos().second) {
-                        while (currentNode->getParent()) {
-                            std::cout << "Pos X " << currentNode->getPos().first << " and Y " << currentNode->getPos().second << std::endl;
-                            currentNode = currentNode->getParent();
+                    if (currentNode->pos.first == endNode->pos.first && currentNode->pos.second == endNode->pos.second) {
+                        while (currentNode->parent) {
+                            std::cout << "Pos X " << currentNode->pos.first << " and Y " << currentNode->pos.second << std::endl;
+                            currentNode = currentNode->parent;
                         }
                         return;
                     }
 
                     std::pair<int, int> successors[4] = {
-                        {currentNode->getPos().first - 1, currentNode->getPos().second},
-                        {currentNode->getPos().first, currentNode->getPos().second - 1},
-                        {currentNode->getPos().first, currentNode->getPos().second + 1},
-                        {currentNode->getPos().first + 1, currentNode->getPos().second}
+                        {currentNode->pos.first - 1, currentNode->pos.second},
+                        {currentNode->pos.first, currentNode->pos.second - 1},
+                        {currentNode->pos.first, currentNode->pos.second + 1},
+                        {currentNode->pos.first + 1, currentNode->pos.second}
                     };
 
                     for (size_t i = 0; i < 4; i++) {
@@ -123,17 +137,17 @@ namespace is
                             continue;
 
                         if (std::find_if(_closeList.begin(), _closeList.end(), [&successors, &i](const std::shared_ptr<Node> &node) -> bool {
-                            return (successors[i].first == node->getPos().first && successors[i].second == node->getPos().second);
+                            return (successors[i].first == node->pos.first && successors[i].second == node->pos.second);
                         }) != _closeList.end())
                             continue;
                         // std::cout << "Free " << successors[i].first << " " << successors[i].second << " " << isValid(successors[i]) << std::endl;
                         
-                        double g = currentNode->getG() + 1;
-                        double h = std::pow(successors[i].first - endNode->getPos().first, 2) + std::pow(successors[i].second - endNode->getPos().second, 2);
+                        double g = currentNode->g + 1;
+                        double h = std::pow(successors[i].first - endNode->pos.first, 2) + std::pow(successors[i].second - endNode->pos.second, 2);
                         double f = g + h;
 
                         if (std::find_if(_openList.begin(), _openList.end(), [&successors, &i, &g](const std::shared_ptr<Node> &node) -> bool {
-                            return (successors[i].first == node->getPos().first && successors[i].second == node->getPos().second && node->getG() < g);
+                            return (successors[i].first == node->pos.first && successors[i].second == node->pos.second && node->g < g);
                         }) != _openList.end())
                             continue;
 
