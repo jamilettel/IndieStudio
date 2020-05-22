e60462527d7ed484c696da72f71af41d9f17d87d
Amaury <amaury.lecomte@epitech.eu>
2020-05-22 13:50:56 +0200

    :construction: Create stats component class

---
+++ src/Components/Stats.cpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2020
+** IndieStudio
+** File description:
+** Stats
+*/
+
+#include "Components/Stats.hpp"
+
+using namespace is::components;
+
+StatsComponent::StatsComponent(
+    std::shared_ptr<is::ecs::Entity> &e,
+    TextComponent &highText,
+    TextComponent &lowText,
+    const std::vector<std::pair<std::string, std::string>> &stats
+) : Component(e), _highText(highText), _lowText(lowText), _stats(stats)
+{
+}
+
+StatsComponent::~StatsComponent()
+{
+}
+
+void StatsComponent::next() noexcept
+{
+    _i++;
+    if (_i == _stats.size())
+        _i = 0;
+}
+
+void StatsComponent::prev() noexcept
+{
+    _i--;
+    if (_i == -1)
+        _i = _stats.size() - 1;
+}
